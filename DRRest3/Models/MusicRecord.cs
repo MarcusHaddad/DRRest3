99d1d668d42e18398a225fd10ce9f882cc0ca2b4
Marcus Michel Haddad <mah045@edu.zealand.dk>
2026-04-13 11:09:50 +0200

    Add project files.

---
+++ DRRest3/Models/MusicRecord.cs
@@ -0,0 +1,11 @@
+﻿namespace DRRest3.Models
+{
+    public class MusicRecord
+    {
+        public string Title { get; set; }
+        public string Artist { get; set; }
+        public int Duration { get; set; } // Duration in seconds
+        public int Year { get; set; }
+
+    }
+}
