a884ee0f3de15e04f080fb3af9455078de89abc0
Marcus Michel Haddad <mah045@edu.zealand.dk>
2026-04-13 11:27:00 +0200

    repository class

---
+++ DRRest3/Models/MusicRecordsRepository.cs
@@ -0,0 +1,19 @@
+﻿namespace DRRest3.Models
+{
+    public class MusicRecordsRepository
+    {
+        private static List<MusicRecord> _musicRecords = new List<MusicRecord>();
+        
+        private static int _nextId = 1;
+        public List<MusicRecord> GetAll()
+        {
+            return new List<MusicRecord>(_musicRecords);
+        }
+        public MusicRecord? GetByTitle(string Title)
+        {
+            return _musicRecords.FirstOrDefault(m => m.Title == Title);
+        }
+
+
+    }
+}
